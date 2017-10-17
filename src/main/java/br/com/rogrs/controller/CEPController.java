24745b57eaab1156975ecced66c1b7c3b01307f7
Rogerio Ribeiro <rogerio.rs@gmail.com>
2017-10-17 01:17:14 -0200

    Ajuste na formatação

--- src/main/java/br/com/rogrs/controller/CEPController.java
+++ src/main/java/br/com/rogrs/controller/CEPController.java
@@ -14,29 +14,29 @@ import br.com.rogrs.service.ViaCEPService;
 @RestController
 @RequestMapping("/buscar")
 public class CEPController {
-	
+
 	@Autowired
 	private ViaCEPService viaCEPService;
 
-    @RequestMapping("/cep")
-    public ResponseEntity<Endereco> buscarCEP(@RequestParam(value="cep") String cep) {
-        
-    	Endereco endereco =  viaCEPService.buscaCEP(cep);
-    	
-    	if (null == endereco || endereco.isErro()) {
-            throw new ResourceNotFoundException("CEP não encontrado!");
-        }
-    	
-    	
-        return new ResponseEntity<>(endereco, HttpStatus.OK);
-    }
-    @ResponseStatus(value = HttpStatus.NOT_FOUND)
-    public class ResourceNotFoundException extends RuntimeException
-    {       
-        private static final long serialVersionUID = -6252766749487342137L;    
-        public ResourceNotFoundException(String message) {
-            super(message);
-        }    
-    }
+	@RequestMapping("/cep")
+	public ResponseEntity<Endereco> buscarCEP(@RequestParam(value = "cep") String cep) {
+
+		Endereco endereco = viaCEPService.buscaCEP(cep);
+
+		if (null == endereco || endereco.isErro()) {
+			throw new ResourceNotFoundException("CEP não encontrado!");
+		}
+
+		return new ResponseEntity<>(endereco, HttpStatus.OK);
+	}
+
+	@ResponseStatus(value = HttpStatus.NOT_FOUND)
+	public class ResourceNotFoundException extends RuntimeException {
+		private static final long serialVersionUID = -6252766749487342137L;
+
+		public ResourceNotFoundException(String message) {
+			super(message);
+		}
+	}
 
 }
