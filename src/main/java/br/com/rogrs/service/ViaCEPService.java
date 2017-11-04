cb245d9005ac27f6720817931342508298827e70
Rogerio Ribeiro <rogerio.rs@gmail.com>
2017-11-04 12:19:09 -0200

    Ajustes de configurações e imports

--- src/main/java/br/com/rogrs/service/ViaCEPService.java
+++ src/main/java/br/com/rogrs/service/ViaCEPService.java
@@ -3,7 +3,6 @@ package br.com.rogrs.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
